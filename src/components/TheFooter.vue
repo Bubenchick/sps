ecb32928253b221dfc5e40928e2e881113911f74
Bubenchick <34536807+Bubenchick@users.noreply.github.com>
2025-04-07 12:46:27 +0300

    Project clear

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer>bye</footer>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped></style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="60" height="60" />
+
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+</template>
+
+<script lang="ts" setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<style scoped></style>
